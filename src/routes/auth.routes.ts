c47ccfefab86ddeb9fcd40e9f98a65632cbbc6a4
kennedymakokha <katchibo2@gmail.com>
2025-04-21 11:48:22 +0300

    fetching the error as a string

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -2,56 +2,15 @@ import { Router } from "express";
 import { register, login, logout, refresh, session_Check, updatePassword, activateuser, requestToken, verifyuser, get_Users, registerAdmin, admin_login, get_Admins } from "../controllers/auth.controller";
 
 const router = Router();
-/**
- * @swagger
- * /api/auth/register:
- *   get:
- *     summary: register  a new user
- *     responses:
- *       200:
- *         description: register User
- */
+
 router.post("/register", register);
-/**
- * @swagger
- * /api/auth/login:
- *   post:
- *     summary: login user
- *     responses:
- *       200:
- *         description: login User
- */
+
 router.post("/login", login);
-/**
- * @swagger
- * /api/auth:
- *   get:
- *     summary: return authenticated User
- *     responses:
- *       200:
- *         description: return logged in user
- */
+
 router.get("/", session_Check);
 router.post("/refresh", refresh);
 router.post("/reset-password", updatePassword);
-/**
- * @swagger
- * /api/auth/reset-password:
- *   get:
- *     summary: Get user by ID
- *     parameters:
- *       - in: path
- *         name: id
- *         required: true
- *         schema:
- *           type: string
- *         description: The user ID
- *     responses:
- *       200:
- *         description: A user object
- *       404:
- *         description: User not found
- */
+
 router.post("/activate-user", activateuser);
 router.post("/verify-otp", verifyuser);
 router.post("/request-otp", requestToken);
--- src/utils/custom_error.util.ts
+++ src/utils/custom_error.util.ts
@@ -2,10 +2,10 @@ export const CustomError = (validator: any, body: any, res: any, next?: any) => 
     const { errors, isValid } = validator(body);
     if (!isValid) {
         let error = Object.values(errors)[0];
-        res.status(400).json({ message: error });
+        res.status(400).json(error);
         return
     } else {
-      
+
         return
     }
 }
